1242bc6931716a946083e196d469913e3d13fd9c
User <User@LAB3-3>
2024-01-18 14:29:48 +0200

    kkkkk

--- NodeInserts/Program.cs
+++ NodeInserts/Program.cs
@@ -5,6 +5,18 @@ namespace NodeInserts
 {
     internal class Program
     {
+        public static bool IsExist(Node<ShabatRecievers> node, int day, int month, int year)
+        {
+            while (node != null)
+            {
+                if(node.GetValue().GetDay()==day && node.GetValue().GetMonth() == month && node.GetValue().GetYear() == year)
+                    return true;
+                node = node.GetNext();
+            }
+            return false;
+        }
+
+
         static void Main(string[] args)
         {
             Node<ShabatRecievers> shabatShalom = new Node<ShabatRecievers>(new ShabatRecievers("Shiri","Shira",18,1,2024));
